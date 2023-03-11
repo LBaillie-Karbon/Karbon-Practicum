fbfc9f3ea24cb5f90478b5bb6aad573f4d04e6dd
Saadat Qadri <saadat@karbonhq.com>
2023-03-11 19:51:53 -0400

    wip.;

--- index.py
+++ index.py
@@ -1,30 +1,38 @@
 from flask import Flask, request, jsonify
-from models import train_model
+from .models import run_model
 import logging
 
 logging.basicConfig(level=logging.DEBUG)
-logging.debug('This will get logged')
+logging.debug("This will get logged")
 
 app = Flask(__name__)
 
-priority_scores = train_model()
-logging.info(priority_scores)
+priority_scores, trained_models = run_model()
+logging.info(trained_models)
 
 # Define your API endpoint
 @app.route("/")
 def hello_world():
     return "Hello!"
 
-@app.route('/priority_scores/<tenant_perma_key>/<perma_key>', methods=['GET'])
+
+@app.route("/priority_scores/<tenant_perma_key>/<perma_key>", methods=["GET"])
 def get_priority_scores(tenant_perma_key, perma_key):
-    logging.info(f"Attempted to retrieve priority score for perma key : {perma_key}")
+    logging.info(
+        f"Attempted to retrieve priority score for email perma key: {perma_key}, tenant_perma_key: {tenant_perma_key}"
+    )
+
+    print(priority_scores.head())
+    print(trained_models.head())
 
     # Check if the perma key exists
-    if not priority_scores.get(perma_key):
-        logging.debug(f"The perma key, {perma_key}, was not found")
-        return -1    
+    try:
+        if priority_scores.get(perma_key):
+            logging.debug(f"The perma key, {perma_key}, was found")
+            return jsonify(priority_scores.get(perma_key))
+    except Exception as e:
+        logging.error(e)
 
-    return jsonify(priority_scores.get(perma_key))
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
